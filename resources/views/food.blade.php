20f52fa35bc4cdada67d30b2514dfd4ffa6fe873
rekayudha13 <yudhareka0@gmail.com>
2022-05-09 20:22:35 +0700

    uts-coba

---
+++ resources/views/food.blade.php
@@ -0,0 +1,5 @@
+@extends('template.main')
+
+@section('body')
+    <h1>Halaman Food</h1>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('template.main')
+
+@section('body')
+    <h1>Halaman Home</h1>
+@endsection
